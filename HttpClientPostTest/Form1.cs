123a5fbf826d0bb34dda4fb0b56cc9b2acf51f0b
Jim Rutt <jimrutt@gmail.com>
2018-08-10 16:19:44 -0400

    cleanenup obsolete code

--- HttpClientPostTest/Form1.cs
+++ HttpClientPostTest/Form1.cs
@@ -23,14 +23,6 @@ namespace HttpClientPostTest
         private async void button1_Click(object sender, EventArgs e)
         {
 
-            /* var replyString = SendTelegramMessage("testing post");
-            string rs = ConvertUnicodeToAscii(replyString.ToString());
-
-            Console.WriteLine("from HERE> " + rs);
-
-            nop();
-            */
-
             List<String> contentList = new List<string>();
 
             contentList = MakeStringList();
@@ -51,30 +43,11 @@ namespace HttpClientPostTest
             
             return returnList;
         }
-        public async Task<string> SendTelegramMessage(string text)
-        {
-            using (var client = new HttpClient())
-            {
-
-                Dictionary<string, string> dictionary = new Dictionary<string, string>();
-                dictionary.Add("PARAM1", "VALUE1");
-                dictionary.Add("PARAM2", text);
-
-                string json = JsonConvert.SerializeObject(dictionary);
-                var requestData = new StringContent(json, Encoding.UTF8, "application/json");
-
-                var response = await client.PostAsync(String.Format("http://localhost:7071/api/DirtyRCV?name=XXX"), requestData);
-
-                
-                var result = await response.Content.ReadAsStringAsync();
-                
-                return result;
-            }
-        }
+       
         private async Task PostBasicAsync(object content, CancellationToken cancellationToken)
         {
             using (var client = new HttpClient())
-            using (var request = new HttpRequestMessage(HttpMethod.Post, "http://localhost:7071/api/DirtyRCV"))
+            using (var request = new HttpRequestMessage(HttpMethod.Post, "https://yikesyikesyikes.azurewebsites.net/api/DirtyRCV"))
             {
                 var json = JsonConvert.SerializeObject(content);
                 using (var stringContent = new StringContent(json, Encoding.UTF8, "application/json"))
@@ -105,31 +78,7 @@ namespace HttpClientPostTest
 
         }
 
-        public string ConvertUnicodeToAscii(System.String unicodeString)
-        {
-            
-            {
-                
-
-                // Create two different encodings.
-                Encoding ascii = Encoding.ASCII;
-                Encoding unicode = Encoding.Unicode;
-
-                // Convert the string into a byte array.
-                byte[] unicodeBytes = unicode.GetBytes(unicodeString);
-
-                // Perform the conversion from one encoding to the other.
-                byte[] asciiBytes = Encoding.Convert(unicode, ascii, unicodeBytes);
-
-                // Convert the new byte[] into a char[] and then into a string.
-                char[] asciiChars = new char[ascii.GetCharCount(asciiBytes, 0, asciiBytes.Length)];
-                ascii.GetChars(asciiBytes, 0, asciiBytes.Length, asciiChars, 0);
-                string asciiString = new string(asciiChars);
-
-                return asciiString;
-            }
-
-        }
+       
 
 
     }
